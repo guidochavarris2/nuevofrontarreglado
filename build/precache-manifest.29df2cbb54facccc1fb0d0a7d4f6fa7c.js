906e947f040cdb3843d2c39733afa6943ebf1934
guidochavarris2 <lucanalopezguido@gmail.com>
2024-10-02 09:38:38 -0600

    first commit

---
+++ build/precache-manifest.29df2cbb54facccc1fb0d0a7d4f6fa7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfae495b9f64c4b417d05ab5c9aeff5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd416e14b5e1b05e8277",
+    "url": "/static/css/2.d8f06225.chunk.css"
+  },
+  {
+    "revision": "d549da701264f10ce443",
+    "url": "/static/css/main.386f097c.chunk.css"
+  },
+  {
+    "revision": "fd416e14b5e1b05e8277",
+    "url": "/static/js/2.1a888cf5.chunk.js"
+  },
+  {
+    "revision": "d549da701264f10ce443",
+    "url": "/static/js/main.2c3e17e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "782adc2b6062ab00461359da5b02b753",
+    "url": "/static/media/Profile.782adc2b.png"
+  },
+  {
+    "revision": "6252f6a473569e4a2b3d531d3d170be3",
+    "url": "/static/media/logo.6252f6a4.png"
+  },
+  {
+    "revision": "5a54ba4b2a91f1fadcc9ddf56542f50b",
+    "url": "/static/media/neonportada.5a54ba4b.jpg"
+  }
+]);
